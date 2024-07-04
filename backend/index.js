c142231809ea7168a699efd3d552930f55574a22
Kamar <57263951+Kamaruddheen@users.noreply.github.com>
2024-07-04 16:35:28 +0530

    Implemented CORS policy to allow all and custom origins
    Configured CORS (Cross-Origin Resource Sharing) policies to allow requests from  custom specified origins.

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,29 @@
+// Packages
 import express from "express";
-import { PORT, mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
+import cors from "cors";
+
+// Local Imports
 import booksRoute from "./routes/bookRoute.js";
+import { PORT, mongoDBURL } from "./config.js";
 
 // Initialize Express app
 const app = express();
 
 // Middleware to parse JSON bodies
 app.use(express.json());
+
+// Middleware to handling CORS POLICY
+// ? OPTION 1: Allow All Origins with Default of cors(*)
+// * app.use(cors());
+// ? OPTION 2: Allow Custom Origins
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type"],
+  })
+);
 
 // Default route (localhost/homepage)
 app.get("/", (request, response) => {
